fe6e100e57b9f758d5dd805a2fe4269e4e566cee
Ben Yu <bennettyu82@icloud.com>
2024-02-14 18:32:59 -0500

    the buttons in the bottom right now

--- assistantapp/ButtonFrame.java
+++ assistantapp/ButtonFrame.java
@@ -10,12 +10,26 @@ public class ButtonFrame extends JFrame implements ActionListener{
 		ButtonFrame(){
 			
 			button = new JButton(); 
-			button.setBounds(200, 100, 100, 50); // gives it some size
+			
+			button.setBounds(400, 600, 50, 50); // gives it some size
+			
+			button.setText("Send");
+			button.setFocusable(false);
+			
+			button.setHorizontalTextPosition(JButton.CENTER);
+			button.setVerticalTextPosition(JButton.CENTER);
 			
 			button.addActionListener(this); // yeah i'm him
 			
+			/*  ALTERNATE:
+			 *  
+			 *  button.addActionListener(e -> (action));
+			 *  button.addActionListener(e -> System.out.println("clicked");
+			 */
+			
 			this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // the X button works
 			//this.setLayout(null);
+			this.setResizable(false);
 			this.setSize(500, 700); // gives the FRAme some size
 			this.setVisible(true); // makes the frame visible
 			this.add(button); // adds button to the frame
@@ -25,7 +39,6 @@ public class ButtonFrame extends JFrame implements ActionListener{
 		public void actionPerformed(ActionEvent e) {
 			if(e.getSource()==button) {
 				System.out.println("clicked");
-			}
-			
+			}			
 		}
 }
